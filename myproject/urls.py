1d3d83ea7499501dfbbfac71c17b98302751bc33
oowada takuji <jiinyo829@gmail.com>
2020-07-15 18:26:26 +0900

    簡単なAPIサーバー作成

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
 from rest_framework import routers
+from myapp import urls
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('api/', include(router.urls)),
+    url('api/', include(urls, namespace='spot')),
 ]

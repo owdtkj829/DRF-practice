060db9dab86317dcd6099433095016f89243c008
oowada takuji <jiinyo829@gmail.com>
2020-07-15 15:34:52 +0900

    DRF設定

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('api/', include(router.urls)),
 ]
